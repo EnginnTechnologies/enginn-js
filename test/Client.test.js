9b7863a07dbf20755519e407ad13cfd7ead79c66
Yann Hourdel <yann@hourdel.fr>
2023-01-03 15:38:17 +0100

    more tests

--- test/Client.test.js
+++ test/Client.test.js
@@ -1,5 +1,6 @@
 import Client from '../src/Client';
 import Errors from '../src/Errors';
+import ProjectsIndex from '../src/ProjectsIndex';
 
 beforeEach(() => {
   fetch.resetMocks();
@@ -91,6 +92,12 @@ describe('Client', () => {
     const client = new Client();
     client.baseUrl = 'https://mock/';
 
+    it('sends a body', async () => {
+      fetch.mockResponseOnce(req => req.json().then(body => JSON.stringify(body)));
+      const response = await client.patch('/', { data: 42 });
+      expect(response.data).toEqual(42);
+    });
+
     it('allows to override headers', async () => {
       fetch.mockResponseOnce(req => Promise.resolve(JSON.stringify(req.headers.get('Bar'))));
       const response = await client.patch('/', {}, { headers: { Bar: 'foo' } });
@@ -107,5 +114,12 @@ describe('Client', () => {
       const response = await client.delete('/', { headers: { Bar: 'foo' } });
       expect(response).toEqual('foo');
     });
+  });
+
+  describe('.projects', () => {
+    const client = new Client();
+    const projects = client.projects();
+    expect(projects).toBeInstanceOf(ProjectsIndex);
+    expect(Object.is(projects.client, client)).toBe(true);
   });
 });
